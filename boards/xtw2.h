e69cddf2f09a7fbc619899336c1e3ad9f940f786
neg2led <4232981+neg2led@users.noreply.github.com>
2021-08-15 22:23:58 +1000

    fixed the clock problem, hangs on SPI RX...

--- boards/xtw2.h
+++ boards/xtw2.h
@@ -13,22 +13,23 @@
 #define BOARD_RCC_LED                RCC_GPIOB
 #define BOARD_PORT_LED               GPIOB
 #define BOARD_PIN_LED                GPIO7
-#define BOARD_LED_HIGH_IS_BUSY       true
+#define BOARD_LED_HIGH_IS_BUSY       false
 
 #define BOARD_RCC_USB_PULLUP         RCC_GPIOB
 #define BOARD_PORT_USB_PULLUP        GPIOB
 #define BOARD_PIN_USB_PULLUP         GPIO8
 #define BOARD_USB_HIGH_IS_PULLUP     true
 
-/* Currently you can only use SPI1, since it has highest clock. */
+/*
+ * The XTW2 is like the XTW100, but with a GD32F103 and a single pinout for both SPI and I2C.
+ * The GD32F103 is mostly the same as an STM32F103, but its SPI peripherals can all do 18MHz officially,
+ * and SPI1 at least has been seen doing 54MHz in the wild. We don't care about I2C, so, whatever.
+ * VCC and GND are hooked straight to the regulator/board, which is an improvement over the XTW100.
+ */
 
 #define HAS_BOARD_INIT 1
 void board_init (void) {
-// Toggle power to the 25xx flash chip
-// PB2 + PB1 serve as GND supply, GPIO_50_MHZ to increase drive current
-  gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO1|GPIO2);
-  gpio_clear(GPIOB, GPIO1|GPIO2);
-// Set #WP and #HOLD high (PA2|PA3)
+  // Set #WP and #HOLD high (PA2|PA3)
   rcc_periph_clock_enable(RCC_GPIOA);
   gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO2|GPIO3);
   gpio_set(GPIOA, GPIO2|GPIO3);
